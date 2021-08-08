c23c46cf0764afb0a356a354fb484cd9daddbae5
mittalaakash <mit.ash.619@gmail.com>
2021-08-09 00:57:29 +0530

    Initial commit

---
+++ src/Components/ClassCounter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class ClassCounter extends Component {
+  state = {
+    count: 0,
+  };
+  incrementer = () => {
+    this.setState(
+      prevState => ({ count: prevState.count + 1 }),
+      () => {
+        console.log(this.state.count);
+      },
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.incrementer}>
+          Clicked {this.state.count} times
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ClassCounter;
