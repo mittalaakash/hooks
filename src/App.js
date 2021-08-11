b35f7846be021e1602585905381232c4d885ad14
mittalaakash <mit.ash.619@gmail.com>
2021-08-12 00:51:34 +0530

    Data fetching reducer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useReducer } from 'react';
+import React from 'react';
+
 import './App.css';
-import ComponentA from './component/ComponentA';
-import ComponentB from './component/ComponentB';
-import ComponentC from './component/ComponentC';
-
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1;
-    case 'decrement':
-      return state - 1;
-    case 'reset':
-      return initialState;
-
-    default:
-      return state;
-  }
-};
-
-export const CountContext = React.createContext();
+import DataFetchingTwo from './Components/DataFetchingTwo';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div className='App'>
-      <div>{count}</div>
-      <CountContext.Provider value={{ count, dispatch }}>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </CountContext.Provider>
+      <DataFetchingTwo />
     </div>
   );
 }
