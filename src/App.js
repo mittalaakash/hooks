f7fd3be94de281b9d1ddc749a75569aac40c037d
mittalaakash <mit.ash.619@gmail.com>
2021-08-11 21:04:31 +0530

    useref + usecontext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import CounterThree from './Components/CounterThree';
+import ComponentA from './component/ComponentA';
+import ComponentB from './component/ComponentB';
+import ComponentC from './component/ComponentC';
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+export const CountContext = React.createContext();
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className='App'>
-      <CounterThree />
+      <div>{count}</div>
+      <CountContext.Provider value={{ count, dispatch }}>
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </CountContext.Provider>
     </div>
   );
 }
---
+++ src/component/ComponentD.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { CountContext } from '../App';
+
+function ComponentD() {
+  const countContext = useContext(CountContext);
+  return (
+    <div>
+      {' '}
+      {countContext.count}
+      <button onClick={() => countContext.dispatch('increment')}>
+        Increment
+      </button>
+      <button onClick={() => countContext.dispatch('decrement')}>
+        Decrement
+      </button>
+      <button onClick={() => countContext.dispatch('reset')}>Reset</button>
+    </div>
+  );
+}
+
+export default ComponentD;
