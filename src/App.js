7e4e80e2a54473569cd57cc48d49b312dbffff55
mittalaakash <mit.ash.619@gmail.com>
2021-08-11 19:16:59 +0530

    complex state/action reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import CounterOne from './Components/CounterOne';
+import CounterTwo from './Components/CounterTwo';
 
 function App() {
   return (
     <div className='App'>
-      <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
