7cc79fee15543c7e8687f2f60b766f0a131abe94
mittalaakash <mit.ash.619@gmail.com>
2021-08-12 02:32:07 +0530

    memo/callback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import DataFetchingTwo from './Components/DataFetchingTwo';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className='App'>
-      <DataFetchingTwo />
+      <Counter />
     </div>
   );
 }
